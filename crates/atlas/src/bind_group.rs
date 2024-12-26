99332ca288ccf71c1f8bfaba0f4600d605af0290
GlennFolker <glenn.folk08@gmail.com>
2024-12-26 17:13:03 +0700

    More docs. Finished 0.3.0

--- crates/atlas/src/bind_group.rs
+++ crates/atlas/src/bind_group.rs
@@ -1,3 +1,7 @@
+//! Utilities to store and keep track of [`Image`]s as [`BindGroup`]s in the render world.
+//!
+//! See `examples/atlas.rs` for example usage.
+
 use bevy_asset::prelude::*;
 use bevy_ecs::{
     event::EventReader,
@@ -11,12 +15,17 @@ use bevy_render::{
 };
 use bevy_utils::{Entry, HashMap};
 
+/// Extracts [`AssetEvent<Image>`]s from the main world to the render world.
 #[derive(Resource, Default)]
 pub struct ImageAssetEvents(Vec<AssetEvent<Image>>);
 
+/// Stores [`BindGroup`]s for each [`Image`] on-demand via [`create`](ImageBindGroups::create).
 #[derive(Resource, Default)]
 pub struct ImageBindGroups(HashMap<AssetId<Image>, BindGroup>);
 impl ImageBindGroups {
+    /// Ensures a [`BindGroup`] for a given [`Image`] is created, returning `true` if it exists
+    /// already. Should work in concert with
+    /// [`Vertex::create_batch`](hephae_render::vertex::Vertex::create_batch).
     #[inline]
     pub fn create(
         &mut self,
@@ -34,12 +43,15 @@ impl ImageBindGroups {
         }
     }
 
+    /// Gets the [`BindGroup`] previously created with [`create`](Self::create). Should work in
+    /// concert with [`RenderCommand::render`](bevy_render::render_phase::RenderCommand::render).
     #[inline]
     pub fn get(&self, id: impl Into<AssetId<Image>>) -> Option<&BindGroup> {
         self.0.get(&id.into())
     }
 }
 
+/// Populates [`ImageAssetEvents`].
 pub fn extract_image_events(
     mut events: ResMut<ImageAssetEvents>,
     mut image_events: Extract<EventReader<AssetEvent<Image>>>,
@@ -48,6 +60,7 @@ pub fn extract_image_events(
     images.extend(image_events.read());
 }
 
+/// For each removed [`Image`], remove the [`BindGroup`] in [`ImageBindGroups`] too.
 pub fn validate_image_bind_groups(mut image_bind_groups: ResMut<ImageBindGroups>, mut events: ResMut<ImageAssetEvents>) {
     for event in events.0.drain(..) {
         match event {
